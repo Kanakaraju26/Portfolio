c2f5afae0a665abfcb698ef55a76e84b61b880f0
Kanaka <131859892+Kanakaraju26@users.noreply.github.com>
2024-03-12 18:30:04 +0530

    change the color of HamBurger menu

--- script.js
+++ script.js
@@ -4,16 +4,3 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-
- const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-  
- function handleDarkModeChange(e) {
-   if (e.matches) {
-     document.documentElement.classList.add('dark-mode');
-   } else {
-     document.documentElement.classList.remove('dark-mode');
-   }
- }
-
- handleDarkModeChange(darkModeMediaQuery); 
- darkModeMediaQuery.addEventListener('change', handleDarkModeChange);
